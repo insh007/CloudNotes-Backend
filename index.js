4f1d525fc03233f5440ffb70f2265c5d1ed97167
insh007 <aliinshad9546@gmail.com>
2023-03-09 03:47:32 +0530

    change done in index

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:3000", "https://cloudnotes-app.web.app"]
+}))
 
 mongoose.connect(process.env.MONGO_STRING, {
     useNewUrlParser : true
